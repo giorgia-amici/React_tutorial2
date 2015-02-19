c8e22c129e61298c87bed135a66e34b1aa91ecdb
Giorgia <giorgia1london@gmail.com>
2015-02-19 14:39:27 +0000

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,30 @@ var CommentBox = React.createClass({
 	render: function(){
 		return(
 			<div className="commentBox">
-				Hello, I am a comment Box 
+			<h1> Comments </h1>
+			<CommentList />
+			<CommentForm />
+			</div>
+			);
+	}
+});
+
+
+var CommentList = React.createClass({
+	render: function(){
+		return(
+			<div className = "commentList">
+			Hello, I am a comment list 
+			</div>
+			);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (
+			<div className="commentForm">
+			Hello I am a comment form 
 			</div>
 			);
 	}
